5387a1d06effb20dcf81ddc2438041613913a607
affe334422 <alle07002@utb.vaxjo.se>
2026-04-14 13:43:08 +0200

    n

--- obj/Debug/net8.0-windows/Host_Projekt_FanMe.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Host_Projekt_FanMe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Host_Projekt_FanMe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df4b9780d4031018dce36d8f838dcaae07610c7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c6013ae68105e761b92490c5a6fc8656bc4431")]
 [assembly: System.Reflection.AssemblyProductAttribute("Host_Projekt_FanMe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Host_Projekt_FanMe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
